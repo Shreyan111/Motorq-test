be3a3ef7aacbae236400649c4c4cc00e53c1bba5
Shreyan111 <shreyan.sanyal2020@vitstudent.ac.in>
2022-08-19 06:57:16 +0530

    video link corrected in readme

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -42,7 +42,7 @@ const Form = ({ currentId, setCurrentId }) => {
         return (
             <Paper className={classes.paper} elevation={6}>
                 <Typography variant="h6" align="center">
-                    Please Sign In to create your own memories and like other's memories.
+                    Please Sign In to create your own events and like other's events.
                 </Typography>
             </Paper>
         );
